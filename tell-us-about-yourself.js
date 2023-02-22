169a29e76a12b7da9dbaa843ebed27db393378c9
Rancor38 <zak.rancor@hotmail.com>
2023-02-22 06:32:09 -0800

    I added my answers

--- tell-us-about-yourself.js
+++ tell-us-about-yourself.js
@@ -1,11 +1,12 @@
 // Tell us about yourself 
 
 // 1. Where do you call home? 
-
+// Yorba Linda, CA
 // 2. Have you lived anywhere else? If so where? 
-
+// Yes. Lots of places
 // 3. What was your most recent job? What did you enjoy about that role?
-
+// IA at GA
 // 4. What is your "comfort" food? 
-
+// A chocolate chip cookie
 // 5. What is one thing you find interesting about yourself ( that others might not notice at first) ? 
+// Too many pidgeon facts
