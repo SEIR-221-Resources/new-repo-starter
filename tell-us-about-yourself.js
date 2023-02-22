f7291bdb04a7c20e64718e82c62309c13388392a
tpett20 <thomaspettersen20@gmail.com>
2023-02-22 09:32:11 -0500

    I added answers

--- tell-us-about-yourself.js
+++ tell-us-about-yourself.js
@@ -1,11 +1,12 @@
 // Tell us about yourself 
 
 // 1. Where do you call home? 
-
+// Queens, NY
 // 2. Have you lived anywhere else? If so where? 
-
+// No
 // 3. What was your most recent job? What did you enjoy about that role?
-
+// Owner of a furniture company.
 // 4. What is your "comfort" food? 
-
+// Ravioli
 // 5. What is one thing you find interesting about yourself ( that others might not notice at first) ? 
+// I have been training judo for 12 years.
