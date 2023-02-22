35146a3b268fc90e312e8456b46075ea29d60182
AnthonyScott <anthony.scott1005@gmail.com>
2023-02-22 09:29:48 -0500

    completed TMAYS

--- tell-us-about-yourself.js
+++ tell-us-about-yourself.js
@@ -1,11 +1,12 @@
 // Tell us about yourself 
 
 // 1. Where do you call home? 
-
+Georgia, north of Atl
 // 2. Have you lived anywhere else? If so where? 
-
+Burbank, California
 // 3. What was your most recent job? What did you enjoy about that role?
-
+Security Job, not really - I'm still a security mostly out of necessity
 // 4. What is your "comfort" food? 
-
+Ramen
 // 5. What is one thing you find interesting about yourself ( that others might not notice at first) ? 
+I find it easy to relate to people of a variety of backgrounds
