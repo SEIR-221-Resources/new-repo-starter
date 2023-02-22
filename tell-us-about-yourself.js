0aafcb8f26de8402b860115a1e75daac2cac1ef2
adricastro1 <adricastro1196@gmail.com>
2023-02-22 09:32:22 -0500

    Answered

--- tell-us-about-yourself.js
+++ tell-us-about-yourself.js
@@ -1,11 +1,12 @@
 // Tell us about yourself 
 
 // 1. Where do you call home? 
-
+Fayetteville, NC
 // 2. Have you lived anywhere else? If so where? 
-
+El Salvador, New Jersey, Texas, Maryland, and Florida
 // 3. What was your most recent job? What did you enjoy about that role?
-
+Retail Store Manager and I loved working with my team
 // 4. What is your "comfort" food? 
-
+Street Tacos
 // 5. What is one thing you find interesting about yourself ( that others might not notice at first) ? 
+Grew up in El Salvador
